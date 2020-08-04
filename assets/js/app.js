10baabd879e83a34b5fb06a9050f13bc4e51885b
carol <carolmaria.sepulvedaolivos@cencosud.cl>
2020-08-03 21:52:00 -0400

    se agrega smooth scroll

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,26 @@
 $(document).ready(function() {
+    // Add smooth scrolling to all links
+    $("#menu a").on('click', function(event) {
+  
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+  
+        // Store hash
+        var hash = this.hash;
+  
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+  
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      } // End if
+    });
   // carrusel del portafolio
   $('.carousel').carousel({
       dist:-60,
@@ -15,4 +37,5 @@ $(document).ready(function() {
 function markMenu(btn) {
   $('.btnM').removeClass('active');
   $('#' + btn).addClass('active');
+
 }
