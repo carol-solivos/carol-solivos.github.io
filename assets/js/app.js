c513b7ebf03247b5ec54cb367b4539c5572b1e6c
carol <carolmaria.sepulvedaolivos@cencosud.cl>
2020-08-31 22:27:59 -0400

    se agrega modal a carousel

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,25 +1,29 @@
 $(document).ready(function() {
-    // smooth scroll
-    $("#menu a, #seeMore a").on('click', function(event) {
-      if (this.hash !== "") {
-        event.preventDefault();
-        var hash = this.hash;
-        $('html, body').animate({
-          scrollTop: $(hash).offset().top
-        }, 800, function(){
-          window.location.hash = hash;
-        });
-      }
-    });
+  $('.modal').modal();
+
+  // smooth scroll
+  $("#menu a, #seeMore a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
   // carrusel del portafolio
   $('.carousel').carousel({
       dist:-60,
+      padding: 80,
       indicators: true
     });
 
   $('.btnM').click(x => {
     markMenu(x.target.id);
   })
+
 
 });
 
