b62d78b3c84b5423a90793894bf64c44008c609e
carol <carol.solivos@gmail.com>
2023-05-01 22:30:15 -0400

    se elimina material y jqery

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,66 +1,87 @@
-$(document).ready(function() {
-  $('.modal').modal();
+let enJSON;
+let esJSON;
 
-  // smooth scroll
-  $("#menu a, #seeMore a").on('click', function(event) {
-    if (this.hash !== "") {
-      event.preventDefault();
-      var hash = this.hash;
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-        window.location.hash = hash;
-      });
-    }
-  });
-  // carrusel del portafolio
-  $('.carousel').carousel({
-      dist:-60,
-      padding: 80,
-      indicators: true
-    });
-
-  $('.btnM').click(x => {
-    markMenu(x.target.id);
-  })
-
-
+fetch("./assets/js/translate/en.json")
+  .then((res) => res.json())
+  .then((data) => {
+    enJSON = data;
+});
+fetch("./assets/js/translate/es.json")
+  .then((res) => res.json())
+  .then((data) => {
+    esJSON = data;
 });
 
-$(window).scroll(function() {
+function setLang() {
+  let translateList = document.getElementsByClassName("cs_translate");
+  for (let item of translateList) {
+    item.innerHTML = data[item.dataset.translate];
+  }
+}
+
+// $(window).scroll(function() {
   
-  // selectors
-  var $window = $(window),
-      $body = $('body'),
-      $panel = $('.panel');
+//   // selectors
+//   var $window = $(window),
+//       $body = $('body'),
+//       $panel = $('.panel');
   
-  // Change 33% earlier than scroll position so colour is there when you arrive.
-  var scroll = $window.scrollTop() + ($window.height() / 3);
+//   // Change 33% earlier than scroll position so colour is there when you arrive.
+//   var scroll = $window.scrollTop() + ($window.height() / 3);
  
-  $panel.each(function () {
-    var $this = $(this);
+//   $panel.each(function () {
+//     var $this = $(this);
     
-    // if position is within range of this panel.
-    // So position of (position of top of div <= scroll position) && (position of bottom of div > scroll position).
-    // Remember we set the scroll to 33% earlier in scroll var.
-    if ($this.position().top <= scroll && $this.position().top + $this.height() > scroll) {
+//     // if position is within range of this panel.
+//     // So position of (position of top of div <= scroll position) && (position of bottom of div > scroll position).
+//     // Remember we set the scroll to 33% earlier in scroll var.
+//     if ($this.position().top <= scroll && $this.position().top + $this.height() > scroll) {
           
-      // Remove all classes on body with color-
-      $body.removeClass(function (index, css) {
-        return (css.match (/(^|\s)color-\S+/g) || []).join(' ');
-      });
+//       // Remove all classes on body with color-
+//       $body.removeClass(function (index, css) {
+//         return (css.match (/(^|\s)color-\S+/g) || []).join(' ');
+//       });
        
-      // Add class of currently active div
-      $body.addClass('color-' + $(this).data('color'));
-      markMenu($(this).data('active'));
-    }
-  });    
+//       // Add class of currently active div
+//       $body.addClass('color-' + $(this).data('color'));
+//       markMenu($(this).data('active'));
+//     }
+//   });    
   
-}).scroll();
+// }).scroll();
 
 // cambia los estilos del menú
-function markMenu(btn) {
-  $('.btnM').removeClass('active');
-  $('#' + btn).addClass('active');
+// document.body.classList.add('color-' + $(this).data('color'))
+// $body.addClass('color-' + $(this).data('color'));
+//       markMenu($(this).data('active'));
 
+function markMenu(btn) {
+  let btnMenu = document.getElementsByClassName('btnMenu');
+  let btnID = document.getElementById(btn);
+
+  for (let btn of btnMenu) {
+    btn.classList.remove('active');
+  }
+  btnID.classList.add('active');
+}
+
+const sections = document.getElementsByClassName("panel");
+
+const animation = (entries, observador) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      console.log(entry.target, entry.target.dataset.active);
+      markMenu(entry.target.dataset.active);
+    }
+  });
+};
+
+const observador = new IntersectionObserver(animation, {
+  root: null,
+  rootMargin: "50px",
+  threshold: 0.3,
+});
+
+for (let section of sections) {
+  observador.observe(section);
 }
