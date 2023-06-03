2a9d737d5311c08ebb32db0005e85f84e6eb2d31
carol <carol.solivos@gmail.com>
2023-06-03 19:54:35 -0400

    new icons, icon animation

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,42 +19,6 @@ function setLang() {
   }
 }
 
-// $(window).scroll(function() {
-  
-//   // selectors
-//   var $window = $(window),
-//       $body = $('body'),
-//       $panel = $('.panel');
-  
-//   // Change 33% earlier than scroll position so colour is there when you arrive.
-//   var scroll = $window.scrollTop() + ($window.height() / 3);
- 
-//   $panel.each(function () {
-//     var $this = $(this);
-    
-//     // if position is within range of this panel.
-//     // So position of (position of top of div <= scroll position) && (position of bottom of div > scroll position).
-//     // Remember we set the scroll to 33% earlier in scroll var.
-//     if ($this.position().top <= scroll && $this.position().top + $this.height() > scroll) {
-          
-//       // Remove all classes on body with color-
-//       $body.removeClass(function (index, css) {
-//         return (css.match (/(^|\s)color-\S+/g) || []).join(' ');
-//       });
-       
-//       // Add class of currently active div
-//       $body.addClass('color-' + $(this).data('color'));
-//       markMenu($(this).data('active'));
-//     }
-//   });    
-  
-// }).scroll();
-
-// cambia los estilos del menú
-// document.body.classList.add('color-' + $(this).data('color'))
-// $body.addClass('color-' + $(this).data('color'));
-//       markMenu($(this).data('active'));
-
 function markMenu(btn) {
   let btnMenu = document.getElementsByClassName('btnMenu');
   let btnID = document.getElementById(btn);
