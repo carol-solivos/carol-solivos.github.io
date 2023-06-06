f9c2c0519037b51c76260f6291d7df10bb8410d9
carol <carol.solivos@gmail.com>
2023-06-06 17:17:40 -0400

    tab projects function and content, splidejs, headers

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,9 +8,12 @@ let esIcon = document.getElementsByClassName("cso-es");
 // modals
 let modalTriggers = document.querySelectorAll('[data-modal]');
 let modalProject = document.getElementById("modalProject");
+let modalProjectContent = document.getElementsByClassName("cso_modal__body__preview__project");
 
 esBtn.addEventListener("click", function(){changeLang("es")}, false);
 enBtn.addEventListener("click", function(){changeLang("en")}, false);
+
+
 
 fetch("./assets/js/translate/en.json")
   .then((res) => res.json())
@@ -63,6 +66,42 @@ function showModal(modalId) {
 
 function closeModal(modalId) {
   document.getElementById(modalId).classList.remove("cso_show")
+}
+
+let sliderProject = document.getElementsByClassName("cso_slider--project");
+
+if (sliderProject) {
+  for (let item of sliderProject) {
+    new Splide(`#${item.id}`, {
+      perPage: 3,
+      perMove: 1,
+      breakpoints: {
+        768: {
+          perPage: 2,
+          padding: {
+            right: "2rem",
+          },
+        },
+      },
+      gap: 5,
+      drag: true,
+      arrows: true,
+      autoplay: false,
+      pagination: true,
+      type: "loop",
+    }).mount();
+  }
+}
+
+function showProject(modalId) {
+  for (let item of modalProjectContent) {
+    item.classList.remove("cso_show");
+  }
+  document.getElementById(modalId).classList.add("cso_show");
+  setTimeout(() => {    
+
+  }, 1000);
+
 }
 
 // const sections = document.getElementsByClassName("panel");
---
+++ assets/js/translate/en.js
@@ -0,0 +1,10 @@
+let data = {
+    "title": "Carol's Portfolio",
+    "profession": "Front-end Developer",
+    "projects": "Projects",
+    "name": "Name",
+    "skills": "Skills",
+    "about": "About me"
+}
+
+export default data;
